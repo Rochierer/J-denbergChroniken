7c6182b82f0e2622ef9d54f3223f781649d5c879
Rochierer <henning.dietz2004@gmail.com>
2024-12-29 19:23:07 +0100

    Fix type error in [slug]/page.tsx

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -8,10 +8,8 @@ const options = { next: { revalidate: 30 } };
 
 export default async function PostPage({
   params,
-}: {
-  params: { slug: string };
-}) {
-  const post = await client.fetch<SanityDocument>(POST_QUERY, params, options);
+}: Readonly<{ params: { slug: string } }>) {
+  const post = await client.fetch<SanityDocument>(POST_QUERY, { slug: params.slug }, options);
 
   return (
     <main className="container mx-auto min-h-screen flex items-center justify-center p-8 bg-gray-100">
